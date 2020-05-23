ffd030e8121b62240577bc009aa58bb877d6d940
Hugo Nogueira <hugomn@gmail.com>
2020-05-23 22:57:19 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,24 @@
+# mtor.io website
+
+<br />
+
+## Quick start
+
+```bash
+# Install packages
+npm install
+
+# Start development server
+gatsby develop
+
+# build
+gatsby build
+```
+
+## License
+
+Copyright 2019 mtor.io
+
+The content of this repository is bound by the following licenses:
+
+- The computer software is licensed under the [MIT](LICENSE) license.
---
+++ src/components/Layout.js
@@ -0,0 +1,39 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { Helmet } from "react-helmet";
+
+import "assets/stylesheets/application.scss";
+
+import Header from "components/Header";
+import Footer from "components/Footer";
+
+const Layout = ({ children, pageName }) => {
+  let className = "";
+
+  if (pageName) {
+    className = `${className} page-${pageName}`;
+  }
+
+  return (
+    <>
+      <Helmet bodyAttributes={{ class: className }}>
+        <title>mtor.io</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap"
+          rel="stylesheet"
+        />
+      </Helmet>
+      <div className="wrapper">
+        <Header />
+        <main>{children}</main>
+        <Footer />
+      </div>
+    </>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
