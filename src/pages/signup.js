ef4678f752fc02a7ea3d4a91d05679ab92ca7fea
Mickey <mickey@Mickeys-MBP.fritz.box>
2020-06-18 12:59:36 +0200

    spacing out the footer

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-
-import Layout from 'components/Layout';
 import Container from 'components/Container';
 import { ReactTypeformEmbed } from 'react-typeform-embed';
 
 const SignupPage = () => {
   return (
-    <Layout pageName="signup">
+  
+    <>
       <Helmet>
         <title>Join us</title>
+        <link
+          href="https://fonts.googleapis.com/css2?family=Muli:ital,wght@0,300;0,400;0,600;0,700;0,800;1,400&display=swap"
+          rel="stylesheet"
+        />
       </Helmet>
+
       <Container>
         <ReactTypeformEmbed url="https://edenwhitcomb.typeform.com/to/ThgIWs" />
       </Container>
-    </Layout>
+    </>
+   
   );
 };
 
